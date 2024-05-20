ca8c117dae62b362e16c48f28bd20b1d873c951e
Daniel Kim <dkim848@aucklanduni.ac.nz>
2024-05-21 11:55:35 +1200

    created methods toString, hashCode, equals in the Country class for the hashSet and printing the country info

--- src/main/java/nz/ac/auckland/se281/Country.java
+++ src/main/java/nz/ac/auckland/se281/Country.java
@@ -11,4 +11,41 @@ public class Country {
     this.continent = continent;
     this.taxFee = taxFee;
   }
+
+  @Override
+  public String toString() {
+    return MessageCli.COUNTRY_INFO.getMessage(country, continent, taxFee);
+  }
+
+  @Override
+  public int hashCode() {
+    int sum = 0;
+    for (Character c : country.toCharArray()) {
+      sum += c * country.indexOf(c);
+    }
+
+    return sum;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    } else if (obj == null) {
+      return false;
+    } else if (getClass() != obj.getClass()) {
+      return false;
+    }
+
+    Country other = (Country) obj;
+    if (country == null) {
+      if (other.country != null) {
+        return false;
+      }
+    } else if (!country.equals(other.country)) {
+      return false;
+    }
+
+    return true;
+  }
 }
